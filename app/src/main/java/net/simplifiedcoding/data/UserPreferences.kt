be24fb90ea7af33795b91632d64887baf35e3716
xiet26 <fcb.messi97@gmail.com>
2020-12-23 00:16:23 +0700

    refactor: update implementation

--- app/src/main/java/net/simplifiedcoding/data/UserPreferences.kt
+++ app/src/main/java/net/simplifiedcoding/data/UserPreferences.kt
@@ -1,8 +1,12 @@
 package net.simplifiedcoding.data
 
 import android.content.Context
-import androidx.datastore.DataStore
+import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.*
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.clear
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
